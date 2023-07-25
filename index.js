45ed5b2aac59de9ebd35c82ec29e97b54157289a
Alvaro-Cabrera <alvaro.cabrera@bootcamp-upgrade.com>
2023-07-25 13:02:35 +0200

    comienzo del proyecto de NODE-gimnasio

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-const express = require("express")
-const dotenv = require("dotenv").config()
-const cors = require("cors")
 
-const {connect} = require("./src/utils/db")
-const comidaRoutes = require("./src/api/routes/comida.routes")
-const restaurantesRoutes = require("./src/api/routes/restaurante.routes")
-const userRoutes = require("./src/api/routes/user.routes")
+
 
 const PORT = process.env.PORT;
 const app = express();
 
-connect()
-app.use(cors(
-    {
-        origin:"*",
-        credentials:true
-    }
-))
-app.use(express.json())
 
-app.use("/restaurantes",restaurantesRoutes);
-app.use("/comidas",comidaRoutes);
-app.use("/users",userRoutes);
+app.use(express.json())
 
 app.listen(PORT,() => console.log(`escuchando en el puerto http://localhost:${PORT}`))
 
